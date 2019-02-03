56ce988a7db97098739425abb40c5460b4232c9b
Amir Hossein Ekhlasi <amir.ekhlasi@gmail.com>
2019-02-03 15:26:16 +0330

    updating address

--- HP.py
+++ HP.py
@@ -1,8 +1,8 @@
-BASE_METADATA = "metadata/base_metadata.json"
-BOOKS_METADATA = "metadata/books_metadata.json"
-FEATURES_METADATA = "metadata/features_metadata.json"
-BOOKS_DIR = "books/"
-PARAGRAPH_METADATA = "metadata/paragraphs.npy"
+BASE_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/base_metadata.json"
+BOOKS_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/books_metadata.json"
+FEATURES_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/features_metadata.json"
+BOOKS_DIR = "/mnt/storage1/Data/gutenberg/books/"
+PARAGRAPH_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/paragraphs.npy"
 
 pre_keys = ["is_analysed", "sents_num", "words_num", "tokens_num", "has_dialogue", "whole_dialogue"]
 keys = ["global_id", "book_id", "local_id"] + pre_keys
